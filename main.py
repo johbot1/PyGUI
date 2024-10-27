b56dc3ebd2194bef47ac82c67c2d9de8597fa187
jbotonakis <jbotonakis@carroll.edu>
2024-10-27 12:16:23 -0600

    Created window (Refamiliarize with tkinter)

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("Hello world!")
-print ("Test test")
+import tkinter as tk
+
+mainwindow = tk.Tk()
+
+
+mainwindow.geometry('500x500')
+mainwindow.title('Shop')
+label = tk.Label(mainwindow, text="Wares", font=('Arial', 35))
+label.pack()
+
+if __name__ == '__main__':
+    mainwindow.mainloop()
