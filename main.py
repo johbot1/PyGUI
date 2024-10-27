2b914227aa01837d0eac67238f541faefe15ec54
jbotonakis <jbotonakis@carroll.edu>
2024-10-27 14:00:44 -0600

    Beginning adding frames

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
+#Main.py
+#Author: John Botonakis
+#Desc: Holds the main logic for displaying the window and it's pieces
 import tkinter as tk
 
-mainwindow = tk.Tk()
+#Main window Setup
+window = tk.Tk()
+window.geometry('600x600')
+window.title('Hangman!')
+window.resizable(False, False)
+
+#Centering the window to the screen
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+# Calculate window position
+widthdivisor = 3 #Found this to be good size for the dividing the width to keep window centered
+heightdivisor = 4 #Found this to be good size for the dividing the height to keep window centered
+x = (screen_width - window.winfo_width()) // widthdivisor
+y = (screen_height - window.winfo_height()) // heightdivisor
+
+# Sets window position
+window.geometry(f"+{x}+{y}")
+
+#Main Title
+title_frame = tk.Frame(window, bg="lightblue", width=500, height=50)
+title_frame.pack(side=tk.TOP, fill=tk.X)
+title_label = tk.Label(title_frame, text="HANGMAN", font=("Helvetica", 20))
+title_label.pack(pady=10)
+
+#Hangman Frame
+gallows_frame = tk.Frame(window, bg="lightgreen", width=200, height=200)
 
 
-mainwindow.geometry('500x500')
-mainwindow.title('Shop')
-label = tk.Label(mainwindow, text="Wares", font=('Arial', 35))
-label.pack()
+#Guesses Frame
+guessed_letters_frame = tk.Frame(window, bg="green", width=200, height=50)
+
+
+#Keys
+# alphabet_frame = tk.Frame(window, bg="blue", width=300, height=200)
+# alphabet_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+# alphabet_buttons = []
+# for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+#     button = tk.Button(alphabet_frame, text=letter, width=2)
+#     button.pack(side=tk.LEFT, padx=5, pady=5)
+#     alphabet_buttons.append(button)
+
+
+
+
 
 if __name__ == '__main__':
-    mainwindow.mainloop()
+    window.mainloop()
