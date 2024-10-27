77e34a9843e75d7efcd1c6ca0c8973d4cbdcc6c6
jbotonakis <jbotonakis@carroll.edu>
2024-10-27 16:28:50 -0600

    Adding letter selection

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ window.resizable(False, False)
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 # Calculate window position
-widthdivisor = 3 #Found this to be good size for the dividing the width to keep window centered
-heightdivisor = 4 #Found this to be good size for the dividing the height to keep window centered
+widthdivisor = 2 #Found this to be good size for the dividing the width to keep window centered
+heightdivisor = 8 #Found this to be good size for the dividing the height to keep window centered
 x = (screen_width - window.winfo_width()) // widthdivisor
 y = (screen_height - window.winfo_height()) // heightdivisor
 
@@ -28,23 +28,40 @@ title_label = tk.Label(title_frame, text="HANGMAN", font=("Helvetica", 20))
 title_label.pack(pady=10)
 
 #Hangman Frame
-gallows_frame = tk.Frame(window, bg="lightgreen", width=200, height=200)
+gallows_frame = tk.Frame(window, bg="pink", width=185, height=300, relief=tk.RAISED)
+gallows_frame.place(relx=0.68, rely=0.1)
 
 
 #Guesses Frame
 guessed_letters_frame = tk.Frame(window, bg="green", width=200, height=50)
+guessed_letters_frame.place(relx=0.01, rely=0.1, relwidth=0.5, relheight=0.2)
 
 
 #Keys
-# alphabet_frame = tk.Frame(window, bg="blue", width=300, height=200)
-# alphabet_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-# alphabet_buttons = []
-# for letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-#     button = tk.Button(alphabet_frame, text=letter, width=2)
-#     button.pack(side=tk.LEFT, padx=5, pady=5)
-#     alphabet_buttons.append(button)
+alphabet_frame1 = tk.Frame(window, width=580, height=150)
+alphabet_frame1.place(relx=0.01, rely=0.7)
+alphabet_frame2 = tk.Frame(window, width=580, height=150)
+alphabet_frame2.place(relx=0.05, rely=0.78)
+alphabet_frame3 = tk.Frame(window, width=580, height=150)
+alphabet_frame3.place(relx=0.1, rely=0.86)
+alphabet_buttons = []
 
+line1 = "QWERTYUIOP"
+line2 = "ASDFGHJKL"
+line3 = "ZXCVBNM"
 
+for letter in line1:
+    button = tk.Button(alphabet_frame1, text=letter, width=5)
+    button.pack(side=tk.LEFT, padx=5, pady=5)
+    alphabet_buttons.append(button)
+for letter in line2:
+    button = tk.Button(alphabet_frame2, text=letter, width=5)
+    button.pack(side=tk.LEFT, padx=5, pady=5)
+    alphabet_buttons.append(button)
+for letter in line3:
+    button = tk.Button(alphabet_frame3, text=letter, width=5)
+    button.pack(side=tk.LEFT, padx=5, pady=5)
+    alphabet_buttons.append(button)
 
 
 
